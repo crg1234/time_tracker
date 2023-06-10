9cd7a7b9eed69364e94a88d74fff5f505c64e9dd
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-10 19:44:36 +0200

    updating api key settings in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@ OPENAI_API_KEY = ENV.fetch('OPENAI_API_KEY')
 
 client = OpenAI::Client.new(access_token: OPENAI_API_KEY)
 system_msg = "You are an api that only returns JSON objects. You do not speak natural language.
-
  All the results you return will imitate famous people from the business world in the following structure:
   [
    {
@@ -21,7 +20,7 @@ question = "Give me 5 imaginary names famous business people"
 
 response = client.chat(
   parameters: {
-    model: "gpt-3.5-turbo", # Required.
+    model: "text-davinci-003", # Required.
     messages: [
       { role: "system", content: system_msg },
       { role: "user", content: question }
