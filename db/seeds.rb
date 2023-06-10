236da015624f05f166ec412f70115ff37772fe75
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-10 19:17:47 +0200

    adding seed using ChatGPT

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,38 @@
 require 'json'
 require "openai"
 
+OPENAI_API_KEY = ENV['OPENAI_API_KEY']
+
+# TO CREATE NEW TIME TRACKER USERS - THE PEOPLE WHO LOGIN TO THE APP
+
 client = OpenAI::Client.new(access_token: OPENAI_API_KEY)
-# system_msg = "You are an api that only returns JSON objects. You do not speak natural language.
+system_msg = "You are an api that only returns JSON objects. You do not speak natural language.
 
-#               Specifically, you are an api similar to google maps and you return places based on the user input.
+ All the results you return will imitate famous people from the business world in the following structure:
+  [
+   {
+    'first_name': 'Christopher',
+    'last_name': 'Grant',
+    'email': 'chris@gmail.com'
+   },
+   ...
+  ]"
+question = "Give me 5 imaginary names famous business people"
 
-#               All the places you return will be in the following structure:
-#               [
-#               {
-#               'name': 'Farm & Co Kingscliff',
-#               'address': '529 cudgen road, cudgen, nsw 2487 Australia'
-#               },
-#               ...
-#               ]"
-# question = "Give me 3 restaurants in Sydney"
+response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo", # Required.
+    messages: [
+      { role: "system", content: system_msg },
+      { role: "user", content: question }
+      ], # Required.
+    temperature: 0.7
+  }
+)
 
-# response = client.chat(
-#   parameters: {
-#       model: "gpt-4", # Required.
-#       messages: [
-#         { role: "system", content: system_msg },
-#         { role: "user", content: question }
-#       ], # Required.
-#       temperature: 0.7,
-#   })
+json_array = response.dig("choices", 0, "message", "content")
+time_tracker_user_results = JSON.parse(json_array)
 
-# json_array = response.dig("choices", 0, "message", "content")
-# rest_results = JSON.parse(json_array)
-
-# rest_results.each do |rest|
-#   Restaurant.create!(name: rest["name"], address: rest["address"])
-# end
+time_tracker_user_results.each do |user|
+  User.create!(first_name: user["first_name"], last_name: user["last_name"], email: user["email"])
+end
