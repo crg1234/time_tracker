44309144f6aadf630136b76783ee08d2469f7708
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-12 18:09:10 +0200

    comments out validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,21 +2,20 @@ class Client < ApplicationRecord
   belongs_to :user
   has_many :projects
 
-  validates :first_name, presence: true
-            :uniqueness => { scope: :last_name }
+  # validates :first_name, presence: true
+  #           :uniqueness => { scope: :last_name }
 
-  validates :last_name, presence: true
-            :uniqueness => { scope: :first_name }
+  # validates :last_name, presence: true
+  #           :uniqueness => { scope: :first_name }
 
-  validates :phone, :presence => true,
-                    :numericality => true,
-                    :length => { :minimum => 10, :maximum => 15 }
+  # validates :phone, :presence => true,
+  #                   :numericality => true,
+  #                   :length => { :minimum => 10, :maximum => 15 }
 
-  validates :email, :presence => true,
-                    :uniqueness => true,
-                    :format => { :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  # validates :email, :presence => true,
+  #                   :format => { :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
 
-  validates :address, :presence => true
+  # validates :address, :presence => true
 
 
 
