ccdac986de808e222e68f4bb227e69af28b99f30
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-13 11:33:50 +0200

    updated validations to multiple lines

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,18 +2,18 @@ class Client < ApplicationRecord
   belongs_to :user
   has_many :projects
 
-  # validates :first_name, presence: true
-  #           :uniqueness => { scope: :last_name }
+  validates :first_name, presence: true
+  validates :first_name, uniqueness: { scope: :last_name }
 
-  # validates :last_name, presence: true
-  #           :uniqueness => { scope: :first_name }
+  validates :last_name, presence: true
+  validates :last_name, uniqueness: { scope: :first_name }
 
-  # validates :phone, :presence => true,
-  #                   :numericality => true,
-  #                   :length => { :minimum => 10, :maximum => 15 }
+  validates :phone, presence: true
+  validates :phone, numericality: true
+  validates :phone, length: { :minimum => 10, :maximum => 15 }
 
-  # validates :email, :presence => true,
-  #                   :format => { :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :email, presence: true
+  validates :email, format: { :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
 
-  # validates :address, :presence => true
+  validates :address, presence: true
 end
