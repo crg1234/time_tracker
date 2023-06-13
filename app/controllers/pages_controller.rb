dfa58607f91796e46ad12a3e08ea3c240e521ff0
Juliette Guillaume <glm.juliette@gmail.com>
2023-06-13 17:33:25 +0200

    add invoices list in dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,11 +7,6 @@ class PagesController < ApplicationController
   def dashboard
     @clients = current_user.clients
     @projects = current_user.projects
-<<<<<<< HEAD
     @my_invoices = Invoice.where(project: current_user.projects)
-=======
-    @invoices = current_user.invoices
-    puts @clients
->>>>>>> e3baf0ca692f51ea30aa0587286c640339a36da1
   end
 end
