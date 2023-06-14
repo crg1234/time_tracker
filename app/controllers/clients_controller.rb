868a78eb868e770752ed693c90fb537bf39a896a
Nurra Barry <billing@carbon-saver.com>
2023-06-14 15:48:05 +0200

    dark table and client controller updated

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,4 +7,21 @@ class ClientsController < ApplicationController
     @clients = Client.all
   end
 
-end
+
+  def new
+    @project = Project.find(params[:project_id])
+    @task = Task.new
+  end
+
+  def create
+    # @client = Client.find(params[:client_id]) MUST find a way to check if existing
+    @client = @client.create(client_params)
+    redirect_to client_path(@client)
+  end
+
+  private
+
+  def task_params
+    params.require(:client).permit(:company_name, :first_name, :last_name, :email, :phone_number, :billing_address)
+  end
+  end
