8ca101969c716db8872cb39fe73742778b45186d
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-14 18:16:29 +0200

    saving

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
-    @amount_to_bill = @task.billing_rate * (@task.time_log / 3_600_000) # to convert milliseconds to hours
+    @amount_to_bill = @task.billing_rate * (@task.time_log / 60000) # to convert milliseconds to minutes
   end
 
   def new
