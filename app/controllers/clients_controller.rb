590f6a09287ae76dfd75b36a2761c185e8cb5649
Safia Raza <safiafeyiaz@gmail.com>
2023-06-14 13:56:30 +0200

    client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,4 +7,22 @@ class ClientsController < ApplicationController
     @clients = Client.all
   end
 
+  def new
+    @client = Client.new
+  end
+  def create
+  
+    @client = Client.new(client_params)
+    @client.user = current_user
+    if @client.save
+      redirect_to client_path(@client)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def client_params
+
+    params.require(:client).permit(:first_name, :last_name, :email, :phone_number, :company_name, :billing_address)
+  end
 end
