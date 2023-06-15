abe9ebbf2b84403f15dc7302e8aeec1dacb47982
Safia Raza <safiafeyiaz@gmail.com>
2023-06-15 16:41:59 +0200

    edit-destroy-client

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "home#index"
 
-  get "dashboard", to: "pages#dashboard"
-
+  get "dashboard", to: "pages#dashboard", as: :dashboard
   resources :projects, only: [:index, :show] do
     resources :tasks, only: [:new, :create]
     resources :invoices, only: [:new, :create, :show]
@@ -17,7 +16,8 @@ Rails.application.routes.draw do
   resources :tasks, only: [:show, :create, :update]
   resources :invoices, only: [:show]
 
-  resources :clients, only: [:show, :new, :create] do
+  resources :clients do
     resources :projects, only: [:new, :create]
   end
+
 end
