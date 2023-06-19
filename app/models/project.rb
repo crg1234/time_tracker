5d72999a82bc6397e5bc72af0c9bd744fc16d5e4
Tom Dennis <tom.w.dennis@hotmail.co.uk>
2023-06-19 13:58:41 +0100

    added some validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   belongs_to :client
   has_many :invoices
   has_many :tasks, dependent: :destroy
+  validates :name, presence: true
+  validates :deadline, presence: true
 end
