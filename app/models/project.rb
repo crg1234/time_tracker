c7447644903dfd0bd2df98828e7d5c51a3aec3c0
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-19 22:53:37 +0200

    saving

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,8 @@ class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :name, presence: true
   validates :deadline, presence: true
+
+  def time_counter
+
+  end
 end
