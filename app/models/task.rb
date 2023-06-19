12cc63eb190ee12ac429ce6b7ecacd8d0d968364
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-19 17:50:22 +0200

    moving hint and image url logic to the task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,44 @@
+require "openai"
+
 class Task < ApplicationRecord
   belongs_to :project
+
+  def hint
+    if read_attribute(:hint).present?
+      return read_attribute(:hint)
+    else
+      client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"] )
+      system_msg = "You are a helpful assistant"
+      question = "Please give me a tip of maximum 100 words about how to do #{title} more efficiently"
+
+      response = client.chat(
+        parameters: {
+            model: "gpt-3.5-turbo-16k", # Required.
+            messages: [
+              { role: "system", content: system_msg },
+              { role: "user", content: question }
+            ], # Required.
+            temperature: 0.7,
+            max_tokens: 2000,
+        })
+
+      # response.dig("choices", 0, "message", "content")
+
+      new_hint = response.dig("choices", 0, "message", "content")
+      self.update_columns(hint: new_hint)
+      return new_hint
+    end
+  end
+
+  def image_url
+    if read_attribute(:image_url).present?
+      return read_attribute(:image_url)
+    else
+      client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"] )
+      response = client.images.generate(parameters: { prompt: "Black and White Photograph of Brad Pitt doing #{task.title} without words or letters", size: "256x256" })
+      new_image_url = response.dig("data", 0, "url")
+      self.update_columns(image_url: new_image_url)
+      return new_image_url
+    end
+  end
 end
