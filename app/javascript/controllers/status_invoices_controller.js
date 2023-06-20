b920d1f147a9d6e5ca0f764c7cf091fc8cc132fa
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-20 18:57:12 +0200

    saving

--- app/javascript/controllers/status_invoices_controller.js
+++ app/javascript/controllers/status_invoices_controller.js
@@ -2,9 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="status-invoices"
 export default class extends Controller {
+  static targets = ["colorButton"]
+  static values = {
+    invoiceSent: Boolean,
+    status: Boolean,
+    invoiceId: Number,
+  }
 
   connect() {
-    //console.log("HELLO FROM STATUS INVOICES CONTROLLER")
+    console.log("HELLO FROM STATUS INVOICES CONTROLLER")
+    console.log(this.colorButtonTarget)
+    console.log(this.invoiceSentValue)
+    console.log(this.statusValue)
+    console.log(this.invoiceId)
   }
 
 
