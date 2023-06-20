d50023710b9c3db6691dda621a28ed93e4ab22d6
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-20 10:25:12 +0200

    updated project schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_142036) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_082354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_142036) do
     t.bigint "client_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_time_log", default: 0
+    t.float "total_amount_to_bill", default: 0.0
     t.index ["client_id"], name: "index_projects_on_client_id"
   end
 
