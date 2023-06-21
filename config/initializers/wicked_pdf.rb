862c8569a20968ca6c58e77bbe38119b75826b67
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-21 15:05:18 +0200

    saving

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -15,6 +15,9 @@ WickedPdf.config = {
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
+  exe_path: Gem.bin_path('wkhtmltopdf-heroku', 'wkhtmltopdf-linux-amd64'),
+
+
   # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
   enable_local_file_access: true,
 
