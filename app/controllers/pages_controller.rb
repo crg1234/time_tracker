becad43fcfa6de4ad64da3505e4c192fc9f6963d
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-21 16:24:10 +0200

    saving

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,20 +14,26 @@ class PagesController < ApplicationController
 
 
     # Total billed on dashboard
-    # @total_billing_amount = 0
-    # @my_invoices.each do |invoice|
-    #   @total_billing_amount += invoice.billing_amount
-    # end
+    @total_billing_amount = 0
+    @my_invoices.each do |invoice|
+      if invoice.billing_amount.nil?
+        @total_billing_amount
+      else
+        @total_billing_amount += invoice.billing_amount
+      end
+    end
 
     # # Payment received on dashboard
 
-    # # Total Time Logged on dashboard
-    # # unless project.total_amount_time.nil? do
-    #   @total_time_logged = 0
-    #   @projects.each do |project|
-    #     @total_time_logged += project.total_amount_time
-    #   end
-    # end
+    # Total Time Logged on dashboard
+    @total_time_logged = 0
+    @projects.each do |project|
+      if project.total_amount_time.nil?
+        @total_time_logged
+      else
+        @total_time_logged += project.total_amount_time
+      end
+    end
 
     # Invoices sent on dashboard
     @invoices_array = []
@@ -35,9 +41,37 @@ class PagesController < ApplicationController
       @invoices_array << invoice
     end
 
-    @count_invoices = @invoices_array.length
+    return 0 if @invoices_array.empty?
+
+    @invoices_sent = @invoices_array.length
+
+     # Paid invoices on dashboard
+    @paid_invoices_array = []
+    @my_invoices.each do |invoice|
+      unless invoice.status == false
+        @invoices_array << invoice
+      end
+    end
+
+    @invoices_paid = @paid_invoices_array.length
+
   end
 
   def learning
   end
 end
+
+
+
+
+def total_time_amount
+    total_time_on_invoice = 0
+    self.project.tasks.each do |task|
+      if task.time_log.nil?
+        total_time_on_invoice
+      else
+        total_time_on_invoice += task.time_log
+      end
+    end
+    return total_time_on_invoice
+  end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,19 +1,18 @@
 class Invoice < ApplicationRecord
   belongs_to :project
 
-  def status_check
-    if self.status == false && self.invoice_sent == false
-      "PENDING"
-      # <td><button class="button btn-status pastel-red" data-status-invoices-target="colorButton">Pending</button></td>
-    elsif self.invoice_sent == true && self.status == false
-      "SENT"
-      # <td><button class="button btn-status pastel-orange" data-status-invoices-target="colorButton">Sent</button></td>
-    elsif self.status == true
-      "PAID"
-      # <td><button class="button btn-status pastel-green" data-status-invoices-target="colorButton">Paid</button></td>
-    end
-  end
-
+  # def status_check
+  #   if self.status == false && self.invoice_sent == false
+  #     "PENDING"
+  #     # <td><button class="button btn-status pastel-red" data-status-invoices-target="colorButton">Pending</button></td>
+  #   elsif self.invoice_sent == true && self.status == false
+  #     "SENT"
+  #     # <td><button class="button btn-status pastel-orange" data-status-invoices-target="colorButton">Sent</button></td>
+  #   elsif self.status == true
+  #     "PAID"
+  #     # <td><button class="button btn-status pastel-green" data-status-invoices-target="colorButton">Paid</button></td>
+  #   end
+  # end
 
   def total_billing_amount
     billing_amount = 0
