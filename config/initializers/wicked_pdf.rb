74c2b41369708935866d22ed57d7c44e4392fe3f
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-21 15:33:55 +0200

    saving

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,7 +8,14 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config = {
+
+WickedPdf.config ||= {}
+WickedPdf.config.merge!({
+  layout: "pdf.html.erb",
+})
+
+
+# WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
@@ -16,10 +23,10 @@ WickedPdf.config = {
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # exe_path: Gem.bin_path('wkhtmltopdf-heroku', 'wkhtmltopdf-linux-amd64'),
-   :exe_path => Rails.root.join('bin', 'wkhtmltopdf-i386').to_s,
+  #  :exe_path => Rails.root.join('bin', 'wkhtmltopdf-i386').to_s,
 
   # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
-  enable_local_file_access: true,
+  # enable_local_file_access: true,
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
@@ -30,4 +37,4 @@ WickedPdf.config = {
   # 'xvfb-run' command, in order to simulate an X server.
   #
   # use_xvfb: true,
-}
+# }
