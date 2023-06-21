26b2ccd45752fcd4bb4b183b7bcc3b2d5dcba9ed
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-21 13:40:13 +0200

    saving

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,18 +14,18 @@ class PagesController < ApplicationController
 
 
     # Total billed on dashboard
-    @total_billing_amount = 0
-    @my_invoices.each do |invoice|
-      @total_billing_amount += invoice.billing_amount
-    end
+    # @total_billing_amount = 0
+    # @my_invoices.each do |invoice|
+    #   @total_billing_amount += invoice.billing_amount
+    # end
 
     # Payment received on dashboard
 
-    # Total Time Logged on dashboard
-    @total_time_logged = 0
-    @projects.each do |project|
-      @total_time_logged += project.total_amount_time
-    end
+    # # Total Time Logged on dashboard
+    # @total_time_logged = 0
+    # @projects.each do |project|
+    #   @total_time_logged += project.total_amount_time
+    # end
 
     # Invoices sent on dashboard
     @invoices_array = []
